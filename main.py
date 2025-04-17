463c1c34d5acb92c567d9a4e47761dcfd489c21e
Aiman <aimanelkhamis@yahoo.de>
2025-04-17 11:28:22 +0200

    Bug Fix 5

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ place=st. text_input("Place")
 days=st.slider("Forecast days", 1, 5, 1)
 option=st.selectbox("Select data to view:", ("Temperature", "Sky"))
 
-if place !="" and option!="":
-    if days==1:
-        st.subheader(f"{option} for the next day in {place}")
-    else:
-        st.subheader(f"{option} for the next {days} days in {place}")
-
-
 
 if option=="Temperature" and place!="":
     try:
+        if place != "" and option != "":
+            if days == 1:
+                st.subheader(f"{option} for the next day in {place}")
+            else:
+                st.subheader(f"{option} for the next {days} days in {place}")
         dates, temp, sky = get_data(place, days)
         figure=px.line(x=dates, y=temp, labels={"x": "Dates", "y": "Temperature"})
         st.plotly_chart(figure)
@@ -26,6 +24,11 @@ if option=="Temperature" and place!="":
 
 if option=="Sky":
     try:
+        if place != "" and option != "":
+            if days == 1:
+                st.subheader(f"{option} for the next day in {place}")
+            else:
+                st.subheader(f"{option} for the next {days} days in {place}")
         dates, temp, sky = get_data(place, days)
         cols = st.columns(6)
         image_map = {
